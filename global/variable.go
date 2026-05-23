035ff0a69eabf1266a52c7fe9dc4b0395b23caa0
zheyan11485 <mili44@126.com>
2026-05-24 00:25:10 +0800

    build: upgrade wails v2.11.0 to v2.12.0 and update dependencies
    - Upgrade wails/v2 from v2.11.0 to v2.12.0
    - Add go-toast/v2 as indirect dependency
    - Update wailsjs runtime with notification API support
    - Update wailsjs go binding types
    - Set IsDebug to true for development
    - Update frontend package-lock.json

--- global/variable.go
+++ global/variable.go
@@ -3,12 +3,12 @@ package global
 import "context"
 
 type WebExVar struct {
-	NeededReportPos   int  //需要获取战报的坐标
-	NeedGetReport     bool //是否需要获取战报
-	NeedSyncTeamUser  bool //是否需要同步同盟成员信息
-	BindIpInfo        bool //是否绑定IP信息 开启后将过滤掉其他IP的数据包(特殊情况使用)
-	NeedGetBattleData bool //是否开启抓取详细战报数据 用于抓取队伍
-	NeedPushBookData     bool //是否推送主公簿数据到前端
+	NeededReportPos        int  //需要获取战报的坐标
+	NeedGetReport          bool //是否需要获取战报
+	NeedSyncTeamUser       bool //是否需要同步同盟成员信息
+	BindIpInfo             bool //是否绑定IP信息 开启后将过滤掉其他IP的数据包(特殊情况使用)
+	NeedGetBattleData      bool //是否开启抓取详细战报数据 用于抓取队伍
+	NeedPushBookData       bool //是否推送主公簿数据到前端
 	NeedPushBattleCallData bool //是否推送战役叫阵数据到前端
 }
 
@@ -16,7 +16,7 @@ var ExVar = WebExVar{
 	0, false, false, false, false, false, false,
 }
 
-var IsDebug bool = false
+var IsDebug bool = true
 var Version string = "0.0.4Beta202605030300"
 var OnlySrcIp = ""
 var OnlyDstIp = ""
--- go.mod
+++ go.mod
@@ -5,12 +5,13 @@ go 1.23
 require (
 	github.com/glebarez/sqlite v1.11.0
 	github.com/google/gopacket v1.1.19
-	github.com/wailsapp/wails/v2 v2.11.0
+	github.com/wailsapp/wails/v2 v2.12.0
 	golang.org/x/sys v0.30.0
 	gorm.io/gorm v1.31.1
 )
 
 require (
+	git.sr.ht/~jackmordaunt/go-toast/v2 v2.0.3 // indirect
 	github.com/bep/debounce v1.2.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/glebarez/go-sqlite v1.21.2 // indirect
